1c1917a5e3d1ba3f828d7a1a806b13c9b8ec77b0
Martins98725 <icaro25martins@gmail.com>
2023-08-03 11:17:14 -0300

    refatorando codigo

--- src/main/java/com/example/restwithspringbootandjavaerudio/PersonController.java
+++ src/main/java/com/example/restwithspringbootandjavaerudio/PersonController.java
@@ -4,6 +4,7 @@ import com.example.restwithspringbootandjavaerudio.model.Person;
 import com.example.restwithspringbootandjavaerudio.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,24 +14,25 @@ import java.util.List;
 public class PersonController {
     @Autowired
     private PersonService service;
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") Long id)throws Exception {
        return service.findById(id);
     }
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findByAll(){
         return service.findAll();
     }
-    @RequestMapping(method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody Person person){
         return service.create(person);
     }
-    @RequestMapping(method = RequestMethod.PUT,consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@RequestBody Person person){
         return service.update(person);
     }
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "id") Long id)throws Exception {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id)throws Exception {
          service.delete(id);
+         return ResponseEntity.noContent().build();
     }
 }
