953c854818eaca3a5a71ad423214e25898da86c4
Martins98725 <icaro25martins@gmail.com>
2024-01-29 11:30:39 -0300

    feat: adcionando as classes para teste unitatios e uma classe para mocks de books

---
+++ src/test/java/com/example/restwithspringbootandjavaerudio/MockBook.java
@@ -0,0 +1,4 @@
+package com.example.restwithspringbootandjavaerudio;
+
+public class MockBook {
+}
---
+++ src/test/java/com/example/restwithspringbootandjavaerudio/service/BooksServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.restwithspringbootandjavaerudio.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BooksServiceTest {
+
+    @Test
+    void findBooksById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+}
